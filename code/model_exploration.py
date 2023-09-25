560eabb7436961c94addb446a503a99442f122ef
Didrik Sand <didrik.sand@gmail.com>
2023-09-25 09:17:40 +0200

    FIxed some bugs and added trimmed dataloader

--- code/model_exploration.py
+++ code/model_exploration.py
@@ -6,6 +6,11 @@ from hyperopt import hp
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 import numpy as np
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
+from shap import TreeExplainer
+
+
 # Path: code/model_exploration.py
 
 catboost_space = {
@@ -13,6 +18,14 @@ catboost_space = {
     "learning_rate": hp.loguniform("learning_rate", -3, -2),
     "random_strength": hp.uniform("random_strength", 0, 10),
     "l2_leaf_reg": hp.choice("l2_leaf_reg", range(2, 10)),
+}
+
+xgboost_space = {
+    "max_depth": hp.choice("max_depth", range(1, 20)),
+    "eta": hp.loguniform("eta", -5, -2),
+    "subsample": hp.quniform("subsample", 0.5, 1, 0.05),
+    "n_estimators": hp.choice("n_estimators", range(20, 205, 5)),
+    "colsample_bytree": hp.quniform("colsample_bytree", 0.5, 1, 0.05),
 }
 
 def load_data():
@@ -33,15 +46,15 @@ def load_data():
 def main():
     mass_residuals = load_data()
     SNP_data = mass_residuals.iloc[:, 7:]
-    SNP_data.fillna(0)
+    SNP_data.fillna(0, inplace=True)
     Y = mass_residuals.ID
-
-    testModel(model=CatBoostRegressor,
+    print("Starting model exploration")
+    XGBcv = testModel(model=XGBRegressor(),
               X = SNP_data,
               Y = Y,
-              search_space=catboost_space,
-              name="catboostCV",
-              )
+              search_space=xgboost_space,
+              name="xgboostCV",
+              ).cross_validate()
 
 if __name__ == "__main__":
     main()
--- code/testModel.py
+++ code/testModel.py
@@ -21,12 +21,14 @@ import pickle
 from scipy.stats import pearsonr
 from sklearn.model_selection import KFold
 import os
-
+from sklearn.ensemble import RandomForestRegressor
+from shap import TreeExplainer
 
 
 @dataclass
 class testModel:
-    '''Class for testing a generic model'''
+    """Class for testing a generic model"""
+
     model: object
     X: pd.DataFrame
     Y: pd.Series
@@ -37,53 +39,116 @@ class testModel:
     corr: float = 0
     best_params = {}
     CV_results = {}
-
+    selection_method: str = "shap"
+    hyperCV:bool = False
 
     def cross_validate(self):
-        kf = KFold(n_splits=10,random_state=42)
+        print("Starting cross validation")
+        kf = KFold(n_splits=10, shuffle=True, random_state=42)
         for fold, (train_val_index, test_index) in enumerate(kf.split(self.X)):
-            self.X_train_val, self.X_test = self.X.iloc[train_val_index], self.X.iloc[test_index]
-            self.Y_train_val, self.Y_test = self.Y.iloc[train_val_index], self.Y.iloc[test_index]
-
-            sel = SelectPercentile(f_regression, percentile=10).fit(self.X_train_val, self.Y_train_val) # type: ignore
-            self.X_train_val = sel.transform(self.X_train_val)
-            self.X_test = sel.transform(self.X_test)
+            self.X_train_val, self.X_test = (
+                self.X.iloc[train_val_index],
+                self.X.iloc[test_index],
+            )
+            self.Y_train_val, self.Y_test = (
+                self.Y.iloc[train_val_index],
+                self.Y.iloc[test_index],
+            )
+            print("Fold", fold + 1, "of", kf.get_n_splits(self.X))
+            self.feature_selection()
 
             self.hyperparameter_tuning()
             self.eval()
 
             self.CV_results[fold] = {
-                'scores': self.score,
-                'corrs': self.corr,
-                'best_params': self.best_params,
-                'sel': sel
+                "scores": self.score,
+                "corrs": self.corr,
+                "best_params": self.best_params,
+                "sel": self.sel,
             }
-            print(f"Fold {fold+1} complete")
-        
-        self.best_settings = max(self.CV_results, key=lambda x: self.CV_results[x]['scores'])
-        self.model.set_params(**self.CV_results[self.best_settings]['best_params']) # type: ignore
-        self.best_score = self.CV_results[self.best_settings]['scores']
-        self.best_corr = self.CV_results[self.best_settings]['corrs']
+            print(f"Fold {fold+1} complete, score = {self.score}, corr = {self.corr}")
+
+        self.best_settings = max(
+            self.CV_results, key=lambda x: self.CV_results[x]["scores"]
+        )
+        self.model.set_params(**self.CV_results[self.best_settings]["best_params"])  # type: ignore
+        self.best_score = self.CV_results[self.best_settings]["scores"]
+        self.best_corr = self.CV_results[self.best_settings]["corrs"]
 
         self.save()
 
-    def objective(self,params):
-        '''Objective function for hyperparameter tuning with cross validation'''
-        self.model.set_params(**params) #type: ignore
-        losses = [0]*5
-        kf_tune = KFold(n_splits=5,random_state=42)
-        for tunefold, (train_index,val_index ) in kf_tune.split(self.X_train_val):
-                self.X_train, self.X_val = self.X_train_val[train_index], self.X_train_val[val_index]
-                self.Y_train, self.Y_val = self.Y_train_val[train_index], self.Y_train_val[val_index]
+    def feature_selection(self):
+        print("Starting feature selection")
+        if self.selection_method == "shap":
+            print("Training RF for feature selection")
+            reg = RandomForestRegressor(n_estimators=40, max_depth=4, random_state=42)
+            reg.fit(self.X_train_val, self.Y_train_val)
+            print("Explaining feature importance with SHAP")
+            explainer = TreeExplainer(reg)
+            shap_values = explainer.shap_values(self.X_train_val)
+            feature_importances = np.abs(shap_values).mean(axis=0)
+            sorted_features = self.X_train_val.columns[
+                np.argsort(feature_importances)[::-1]
+            ]
+            num_features = int(len(self.X.columns) * 0.10)
+            self.X_train_val = self.X_train_val[sorted_features[:num_features]]
+            self.X_test = self.X_test[sorted_features[:num_features]]
 
-                self.model.fit(self.X_train,self.Y_train) #type: ignore
-                y_pred = self.model.predict(self.X_val) #type: ignore
-                losses[tunefold] = self.score_func(self.Y_val,y_pred)
+            self.sel = {"method": "shap", "features": sorted_features[:num_features]}
 
-        return {'loss': np.mean(losses),'loss_variance': np.var(losses) ,'status': STATUS_OK}
+        elif self.selection_method == "f_reg":
+            print("Selecting features using f_regression")
+            sel = SelectPercentile(f_regression, percentile=10).fit(self.X_train_val, self.Y_train_val)  # type: ignore
+            self.X_train_val = sel.transform(self.X_train_val)
+            self.X_test = sel.transform(self.X_test)
+            self.sel = {"method": "f_reg", "features": sel.get_support(indices=True)}
+
+    def objective(self, params):
+        """Objective function for hyperparameter tuning with cross validation"""
+        self.model.set_params(**params)  # type: ignore
+        if self.hyperCV:
+            losses = [0] * 5
+            kf_tune = KFold(n_splits=5, shuffle=True, random_state=42)
+            for tunefold, (train_index, val_index) in enumerate(
+                kf_tune.split(self.X_train_val)
+            ):
+                print(
+                    f"Hyperparameter fold {tunefold+1} of {kf_tune.get_n_splits(self.X_train_val)}"
+                )
+
+                self.X_train, self.X_val = (
+                    self.X_train_val.iloc[train_index],
+                    self.X_train_val.iloc[val_index],
+                )
+                self.Y_train, self.Y_val = (
+                    self.Y_train_val.iloc[train_index],
+                    self.Y_train_val.iloc[val_index],
+                )
+
+                self.model.fit(self.X_train, self.Y_train)  # type: ignore
+                self.y_pred = self.model.predict(self.X_val)  # type: ignore
+                try:
+                    losses[tunefold] = self.score_func(self.Y_val, self.y_pred)
+                except:
+                    print("score func not working")
+                    losses[tunefold] = mean_squared_error(self.Y_val, self.y_pred)
+                return {
+                    "loss": np.mean(losses),
+                    "loss_variance": np.var(losses),
+                    "status": STATUS_OK,
+                }
+        else:
+            self.model.fit(self.X_train_val, self.Y_train_val)
+            self.y_pred = self.model.predict(self.X_test)
+            try:
+                losses = self.score_func(self.Y_test, self.y_pred)
+            except:
+                print("score func not working")
+                losses = mean_squared_error(self.Y_test, self.y_pred)
+            return {"loss": losses, "status": STATUS_OK}
 
     def hyperparameter_tuning(self):
-        '''Optimize hyperparameters using bayesian optimization'''
+        """Optimize hyperparameters using bayesian optimization"""
         trials = Trials()
         best = fmin(
             fn=self.objective,
@@ -95,32 +160,40 @@ class testModel:
         self.best_params = best
 
     def eval(self):
-        '''Train model using best hyperparameters'''
-        self.model.set_params(**self.best_params) # type: ignore
-        self.model.fit(self.X_train_val,self.Y_train_val) # type: ignore
-        y_pred = self.model.predict(self.X_test) # type: ignore
-        self.score = self.score_func(self.Y_test,y_pred)
-        self.corr = pearsonr(self.Y_test,y_pred)[0]
+        """Train model using best hyperparameters"""
+        self.model.set_params(**self.best_params)  # type: ignore
+        self.model.fit(self.X_train_val, self.Y_train_val)  # type: ignore
+        self.y_pred = self.model.predict(self.X_test)  # type: ignore
+        try:
+            self.score = self.score_func(self.Y_test, self.y_pred)
+        except:
+            print("score func not working")
+            self.score = mean_squared_error(self.Y_test, self.y_pred)
+
+        self.corr = pearsonr(self.Y_test, self.y_pred)[0]
 
     def save(self):
-        '''Save model to file'''
+        """Save model to file"""
+
+        os.makedirs(f"models/{self.name}", exist_ok=True)
         self.plot()
-        os.makedirs(f'models/{self.name}',exist_ok=True)
         with open(f"models/{self.name}/{self.name}.pkl", "wb") as f:
             pickle.dump(self.model, f)
         with open(f"models/{self.name}/{self.name}_best_sel.pkl", "wb") as f:
             pickle.dump(self.CV_results[self.best_settings]["sel"], f)
 
-        os.makedirs(f'results',exist_ok=True)
+        os.makedirs(f"results", exist_ok=True)
         with open(f"results/results.txt", "a") as f:
-            f.write(f'''{self.name}:\tsocre = {self.best_score}, corr = {self.best_corr}\n''')
-            
+            f.write(
+                f"""{self.name}:\tsocre = {self.best_score}, corr = {self.best_corr}\n"""
+            )
+
     def plot(self):
-        '''Plot results, score and correlation'''
+        """Plot results, score and correlation"""
         plt.figure(1)
         x = self.CV_results.keys()
-        scores = [self.CV_results[key]['scores'] for key in x]
-        corrs = [self.CV_results[key]['corrs'] for key in x]
+        scores = [self.CV_results[key]["scores"] for key in x]
+        corrs = [self.CV_results[key]["corrs"] for key in x]
         plt.title(f"{self.score_func.__name__}")
         plt.plot(x, scores)
         plt.xlabel("Fold")
@@ -133,5 +206,3 @@ class testModel:
         plt.xlabel("Fold")
         plt.grid()
         plt.savefig(f"models/{self.name}/{self.name}_corr.png")
-
-
